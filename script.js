fc935af458011690c6cef4f54d597ce201ca3b8c
Jhonatan <jhonatan.1701621476@ucaldas.edu.co>
2026-08-23 18:49:22 -0500

    modificaciones de apariencia ademas de el meni lateral y la pagina sobre mi con descarga de PDF

--- script.js
+++ script.js
@@ -3,27 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeBtn = document.getElementById('closeBtn');
   const sidebar = document.getElementById('sidebar');
   const sidebarOverlay = document.getElementById('sidebarOverlay');
-  const navLinks = document.querySelectorAll('.nav-link');
 
-  // Abrir Menú
   const openSidebar = () => {
     sidebar.classList.add('active');
     sidebarOverlay.classList.add('active');
   };
 
-  // Cerrar Menú
   const closeSidebar = () => {
     sidebar.classList.remove('active');
     sidebarOverlay.classList.remove('active');
   };
 
-  // Event Listeners para abrir/cerrar
   menuToggle.addEventListener('click', openSidebar);
   closeBtn.addEventListener('click', closeSidebar);
   sidebarOverlay.addEventListener('click', closeSidebar);
-
-  // Cerrar el menú automáticamente al seleccionar una opción
-  navLinks.forEach(link => {
-    link.addEventListener('click', closeSidebar);
-  });
 });
